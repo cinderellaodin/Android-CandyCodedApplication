27c09ae79c99cfdca4f549283f06a8bd7da9927c
cindy <cindycinderella95@gmail.com>
2019-10-30 10:57:14 +0100

    call done

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -45,4 +45,12 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view){
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        Uri callUri = Uri.parse("tel:0123456789");
+        callIntent.setData(callUri);
+
+        startActivity(callIntent);
+    }
 }
